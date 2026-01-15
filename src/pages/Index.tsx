08314419636e3c4f2e639dc26873fb51821ded6b
knitesh026 <kr.nitesh026@gmail.com>
2026-01-15 19:56:08 +0530

    updated

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -106,7 +106,7 @@ const Index = () => {
         </div>
 
         {/* Middle Advertisement Banner */}
-        <div className="w-full flex justify-center my-6 sm:my-8 px-2 sm:px-0">
+        <div className="w-full flex justify-center my-6 sm:my-8 px-2 sm:px-0 ml-96">
           <AdBanner468x60 key="middle-banner" />
         </div>
 
